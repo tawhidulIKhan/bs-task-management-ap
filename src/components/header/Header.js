7e059d839d8fdd7d89db6effc7816b2b8c22d7c3
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-28 20:11:43 +0600

    feat: add header

---
+++ src/components/header/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import './Header.scss';
+
+export default function Header() {
+  return (
+    <div className='header'>
+      <div className='container'>
+        <div className='header__content'>
+          <div className='header__left'>
+            <img src="https://via.placeholder.com/150x50" alt="Logo" />
+          </div>
+          <div className='header__right'>
+            <ul className='header__menu'>
+              <li>
+                <Link className='header__menu__item' to="/dashboard">Dashboard</Link>
+              </li>
+              <li>
+                <Link className='header__menu__item' to="/tasks">Tasks</Link>
+              </li>
+              <li>
+                <Link className='header__menu__item' to="/members">Members</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
