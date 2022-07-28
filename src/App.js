36a4926342f612b79254344c225abeacb34da1c9
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-28 20:12:17 +0600

    feat: add dashboard page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import { Routes, Route, Link } from "react-router-dom";
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </div>
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import DashboardContent from '../components/dashboardContent/DashboardContent'
+import Layout from './../components/layout/Layout'
+export default function Dashboard() {
+  return (
+    <Layout>
+      <DashboardContent />
+    </Layout>
+  )
+}
