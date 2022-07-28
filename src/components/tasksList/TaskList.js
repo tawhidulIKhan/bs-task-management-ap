23286de7748077d1a8b8904f236c32190e97aad6
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-28 20:44:18 +0600

    feat: add task list content table

---
+++ src/components/tasksList/TaskList.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function TaskList() {
+  return (
+    <div className='container'>
+      <div className='page__header'>
+        <h2 className='page__header__title'>All Tasks</h2>
+        <div>
+          <Link to="/tasks/create" className='btn--link'>Create New Task</Link>
+        </div>
+      </div>
+      <div className='page__content'>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Creation date</th>
+              <th>Assigned to</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Reading Books</td>
+              <td>28, July 2022</td>
+              <td>Tawhid</td>
+            </tr>
+              <tr>
+                <td>Reading Books</td>
+                <td>28, July 2022</td>
+                <td>Tawhid</td>
+              </tr>
+              <tr>
+                <td>Reading Books</td>
+                <td>28, July 2022</td>
+                <td>Tawhid</td>
+              </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}
