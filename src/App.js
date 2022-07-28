cea08c84509d2a3ed79a57aecf198b969a593423
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-28 19:31:09 +0600

    feat: add app.scss and login page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
-import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
