03df1d4cc5aa46d1a36b9054e30ee35673624c96
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-28 22:23:33 +0600

    add dummy tasks

--- src/services/api/tasks/request.js
+++ src/services/api/tasks/request.js
@@ -1,11 +1,14 @@
+import { mapTasksFromServerToClient } from './mapper';
+
 /**
  * function to fetch all tasks
  * @param {page,queryparams} request 
  * @returns data, meta
  */
 const all = (request) => {
+  const Tasks = [{id: "1", title: "Task 1", createdAt: "date-111", assignedTo: "Tawhid"}]
   return {
-    data: [],
+    data: mapTasksFromServerToClient(Tasks),
     meta: null
   }
 }
