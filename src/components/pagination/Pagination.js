556c048ca2fef8f3d27860adeb6d0371dca369fb
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-29 23:21:22 +0600

    feat: add pagination

---
+++ src/components/pagination/Pagination.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import usePagination from '../../hooks/usePagination'
+import './Pagination.scss';
+export default function Pagination(props) {
+    const pagination = usePagination({ perPage: props.perPage, total: props.total })
+    const onClickHandler = (page) => {
+        props.onClick(page)
+    }
+    return (
+        <ul className='pagination'>
+            {pagination.map((itr, index) =>
+                <li key={index} className={itr === props.currentPage ? 'pagination__active' : ''}>
+                    <button onClick={() => onClickHandler(itr)}>{itr}</button>
+                </li>
+            )}
+        </ul>
+    )
+}
