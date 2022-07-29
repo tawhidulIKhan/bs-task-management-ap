9062351b020db26fab43e75fe6b635fa9e177f81
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-29 17:00:09 +0600

    add member create page

---
+++ src/pages/MemberCreate.js
@@ -0,0 +1,10 @@
+import Layout from '../components/layout/Layout'
+import MemberForm from '../components/memberForm/MemberForm'
+
+export default function MemberCreate() {
+  return (
+    <Layout>
+      <MemberForm />
+    </Layout>
+  )
+}
