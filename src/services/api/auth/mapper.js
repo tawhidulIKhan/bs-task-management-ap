edef9e5dacc0f858b52746aedc91a0b2b4693912
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-29 11:10:11 +0600

    feat: add login auth data mapper

---
+++ src/services/api/auth/mapper.js
@@ -0,0 +1,10 @@
+/**
+ * function to format login credentials
+ * @param {Login object} auth
+ * @returns formatted login obj
+ */
+
+ export const mapAuthFromServerToClient = (data) => ({
+    token: data.token,
+    user: data.user
+})
