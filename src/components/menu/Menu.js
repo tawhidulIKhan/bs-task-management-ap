e781544a23edb5b66b8ceaf3d3ccafc4282cb54c
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-29 21:16:51 +0600

    refator: seperated menu

---
+++ src/components/menu/Menu.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import endpoints from '../../config/endpoints'
+import './Menu.scss';
+
+export default function Menu() {
+    return (
+        <ul className='menu'>
+            <li>
+                <Link className='menu__item' to={endpoints.DASHBOARD}>Dashboard</Link>
+            </li>
+            <li>
+                <Link className='menu__item' to={endpoints.TASKS}>Tasks</Link>
+            </li>
+            <li>
+                <Link className='menu__item' to={endpoints.MEMBERS}>Members</Link>
+            </li>
+        </ul>
+    )
+}
