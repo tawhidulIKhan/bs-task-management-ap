1f99ec58cac9e7d4ff997075e54f82e70305bc06
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-29 21:28:02 +0600

    refactor: add btnlink to page header

--- src/components/pageHeader/PageHeader.js
+++ src/components/pageHeader/PageHeader.js
@@ -1,15 +1,19 @@
-import { Link } from 'react-router-dom'
-import endpoints from '../../config/endpoints'
+import { Link } from 'react-router-dom';
 import './PageHeader.scss';
 
 export default function PageHeader(props) {
-    const { title, linkLabel, link } = props
-  return (
+    const { title, linkLabel, link, btnLabel, btnLink } = props
+    return (
         <div className='page__header'>
-        <h2 className='page__header__title'>{title}</h2>
-            <div className='page__header__breadcrumb'>
+            <h2 className='page__header__title'>{title}</h2>
+            {link && linkLabel ? (<div className='page__header__breadcrumb'>
                 <Link to={link}>{linkLabel}</Link>
-            </div>
+            </div>) : null
+            }
+            {btnLabel && btnLink ? (<div className='page__header__breadcrumb'>
+                <Link className='btn--link' to={btnLink}>{btnLabel}</Link>
+            </div>) : null
+            }
         </div>
     )
 }
