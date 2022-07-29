fb67aa368e2e2e6b9af05f110de5b824ce511064
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-29 16:39:22 +0600

    add fetching member on member list

--- src/components/memberList/MemberList.js
+++ src/components/memberList/MemberList.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import endpoints from '../../config/endpoints';
+import MemberManager from '../../services/api/members/request';
 
 export default function MemberList() {
+  const [members, setMembers] = useState([])
+  useEffect(() => {
+    fetchTasks();
+  }, [])
+
+  const fetchTasks = async () => {
+    try {
+      const response = await MemberManager.all();
+      setMembers(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className='container'>
       <div className='page__header'>
         <h2 className='page__header__title'>All Members</h2>
         <div>
-          <Link to="/tasks/create" className='btn--link'>Create New Task</Link>
+          <Link to="/tasks/create" className='btn--link'>Add New member</Link>
         </div>
       </div>
       <div className='page__content'>
@@ -15,26 +31,20 @@ export default function MemberList() {
           <thead>
             <tr>
               <th>Title</th>
-              <th>Creation date</th>
-              <th>Assigned to</th>
+              <th>Email</th>
+              <th>Created at</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Reading Books</td>
-              <td>28, July 2022</td>
-              <td>Tawhid</td>
+            {members.map(member => (
+            <tr key={member.id}>
+              <td>
+                <Link to={endpoints.MEMBERS_DETAILS.replace(":id", member.id)}>{member.name}</Link>
+              </td>
+              <td>{member.email}</td>
+              <td>{member.createdAt}</td>
             </tr>
-              <tr>
-                <td>Reading Books</td>
-                <td>28, July 2022</td>
-                <td>Tawhid</td>
-              </tr>
-              <tr>
-                <td>Reading Books</td>
-                <td>28, July 2022</td>
-                <td>Tawhid</td>
-              </tr>
+          ))}
           </tbody>
         </table>
       </div>
