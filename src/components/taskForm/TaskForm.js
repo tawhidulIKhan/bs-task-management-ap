9e816b9c242303d210da2a8b58ba5af96279e48f
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-29 13:53:19 +0600

    complete task create form

--- src/components/taskForm/TaskForm.js
+++ src/components/taskForm/TaskForm.js
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import endpoints from '../../config/endpoints';
 import MemberManager from '../../services/api/members/request';
+import TaskManager from '../../services/api/tasks/request';
 import './TaskForm.scss';
-
+const INITIAL_USER_INPUT = {
+  title: '',
+  description: '',
+  member: null
+}
 export default function TaskForm() {
   const [members, setMembers] = useState([]);
+  const [userInput, setUserInput] = useState(INITIAL_USER_INPUT)
+  const navigate = useNavigate();
   useEffect(() => {
     fetchMembers();
   }, [])
@@ -17,7 +25,25 @@ export default function TaskForm() {
       console.error(error);
     }
   }
+  const createTask = async (ev) => {
+    ev.preventDefault();
+    try {
+      await TaskManager.create(userInput);
+      navigate(endpoints.TASKS);
+    } catch (error) {
+    console.error(error)    
+    }
+  }
+
+  const titleHandler =(ev) => 
+    setUserInput({...userInput, title: ev.target.value})
   
+    const descriptionHandler =(ev) => 
+    setUserInput({...userInput, descriptionHandler: ev.target.value})
+  
+    const memberHandler =(ev) => 
+    setUserInput({...userInput, member: ev.target.value})
+
   return (
     <div className='taskform form'>
       <div className='container-xs'>
@@ -31,19 +57,19 @@ export default function TaskForm() {
           <form className='form'>
             <p className="form__item">
               <label className='form__item__label'>Title</label>
-              <input className='input-field' placeholder='Enter title' />
+              <input onChange={titleHandler} className='input-field' placeholder='Enter title' />
             </p>
             <p className="form__item">
               <label className='form__item__label'>Description</label>
-              <textarea className='input-field' placeholder='Enter your description' />
+              <textarea onChange={descriptionHandler} className='input-field' placeholder='Enter your description' />
             </p>
             <p className="form__item">
               <label className='form__item__label'>Members</label>
-              <select className='input-select'>
+              <select onChange={memberHandler} className='input-select'>
                 {members.map(member => <option key={member.id}>{member.name}</option>)}
               </select>
             </p>
-            <button className='btn--primary'>Create</button>
+            <button onClick={createTask} className='btn--primary'>Create</button>
           </form>
         </div>
       </div>
