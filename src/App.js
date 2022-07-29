061d67f946af7d465a43afcb529f1a37fc42a51a
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-29 17:14:40 +0600

    add member details route

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import TaskDetails from "./pages/TaskDetails";
 import Tasks from './pages/Tasks';
 import 'react-toastify/dist/ReactToastify.css';
 import MemberCreate from "./pages/MemberCreate";
+import MemberDetails from "./pages/MemberDetails";
 
 function App() {
   return (
@@ -44,6 +45,11 @@ function App() {
         <Route path={endpoints.MEMBERS} element={
           <PrivateRoute>
             <Members />
+          </PrivateRoute>
+        } />
+         <Route path={endpoints.MEMBERS_DETAILS} element={
+          <PrivateRoute>
+            <MemberDetails />
           </PrivateRoute>
         } />
         <Route path={endpoints.MEMBERS_CREATE} element={
