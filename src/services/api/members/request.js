88ee5de215c09de272238dd0b44d3721b3c60793
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-29 17:14:24 +0600

    add member show, remove, update

--- src/services/api/members/request.js
+++ src/services/api/members/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import endpoints from '../../../config/endpoints';
 import { getAxiosHeader } from '../../../utils/helpers';
-import { mapMembersFromServerToClient } from './mapper';
+import { mapMembersFromServerToClient, mapMemberFromServerToClient } from './mapper';
 /**
  * function to fetch all members
  * @param {page,queryparams} request 
@@ -35,9 +35,54 @@ const create = async (request) => {
    }
  }
 
+ const update = async (request) => {
+  const response = await axios({
+   method: 'post',
+   url:endpoints.MEMBERS_UPDATE_API.replace(':id', request.id),
+   data: {
+    id: request.id,
+    name: request.name,
+    email: request.email,
+    '_method': 'PUT'
+   },
+   headers: getAxiosHeader()
+  });
+   return {
+     data: response.data,
+   }
+ }
+
+ const show = async (request) => {
+  const response = await axios({
+    method: 'get',
+    url:endpoints.MEMBERS_DETAILS_API.replace(':id', request.id),
+    headers: getAxiosHeader()
+   });
+   return {
+    data: mapMemberFromServerToClient(response.data),
+  }
+}
+
+const remove = async (request) => {
+  const response = await axios({
+    method: 'post',
+    url:endpoints.MEMBERS_DETAILS_API.replace(':id', request.id),
+    data: {
+      '_method': 'DELETE'
+    },
+    headers: getAxiosHeader()
+   });
+   return {
+    data: response.data,
+  }
+}
+
 const MemberManager = {
   all,
-  create
+  create,
+  show,
+  remove,
+  update
 }
 
 export default MemberManager;
