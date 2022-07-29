fe6c7a18a446f1f77513887118565b85427f40fa
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-29 21:17:47 +0600

    refactor: add header to component

---
+++ src/components/header/Header.js
@@ -0,0 +1,23 @@
+import Logo from '../../components/logo/Logo';
+import Menu from '../../components/menu/Menu';
+import MenuProfile from '../../components/menuProfile/MenuProfile';
+import './Header.scss';
+
+export default function Header() {
+  return (
+    <div className='header'>
+      <div className='container'>
+        <div className='header__content'>
+          <div className='header__left'>
+            <Logo />
+          </div>
+          <div className='header__right'>
+            <Menu />
+            <MenuProfile />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
