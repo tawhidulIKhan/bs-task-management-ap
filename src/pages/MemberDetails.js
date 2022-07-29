39d043846564104fd1896e49cb5204aef9c95aa8
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-29 17:13:35 +0600

    add member details

---
+++ src/pages/MemberDetails.js
@@ -0,0 +1,10 @@
+import Layout from '../components/layout/Layout'
+import MemberDetailsContent from '../components/memberDetailsContent/MemberDetailsContent'
+
+export default function MemberDetails() {
+  return (
+    <Layout>
+      <MemberDetailsContent />
+    </Layout>
+  )
+}
