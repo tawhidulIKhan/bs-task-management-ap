8ff4849e343667c04496c63f31176591df3791be
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-29 12:04:08 +0600

    add dashboard route

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -4,6 +4,7 @@ const API_ENDPOINT = process.env.REACT_APP_API_URL;
 const endpoints = {
   LOGIN: "/login",
   LOGIN_API: `${API_ENDPOINT}/login`,
+  DASHBOARD: '/',
   TASKS: "/tasks",
   TASKS_CREATE: "/tasks/create",
   MEMBERS: "members"
