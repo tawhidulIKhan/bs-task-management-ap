507acb3848e9abf8741612703cd187777e351116
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-29 11:08:33 +0600

    feat: add login api

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -1,7 +1,9 @@
 
+const API_ENDPOINT = process.env.REACT_APP_API_URL;
 
 const endpoints = {
   LOGIN: "/login",
+  LOGIN_API: `${API_ENDPOINT}/login`,
   TASKS: "/tasks",
   TASKS_CREATE: "/tasks/create",
   MEMBERS: "members"
