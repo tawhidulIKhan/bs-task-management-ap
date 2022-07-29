138d86c9b56345c951530da1750b6ef058274135
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-29 10:40:29 +0600

    feat: add member list page base

---
+++ src/pages/Members.js
@@ -0,0 +1,10 @@
+import Layout from '../components/layout/Layout'
+import MemberList from '../components/memberList/MemberList'
+
+export default function Members() {
+  return (
+    <Layout>
+      <MemberList />
+    </Layout>
+  )
+}
