f2213466dfaa5bcf9be6af3f6e438cbd770eb3bc
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-29 19:54:51 +0600

    feat: add tile button to dashboard

--- src/components/dashboardContent/DashboardContent.js
+++ src/components/dashboardContent/DashboardContent.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import endpoints from '../../config/endpoints'
+import './DashboardContent.scss';
 
 export default function DashboardContent() {
   return (
-    <div>
+    <div className='dashboard__page'>
       <div className='container'>
-        kk
-      </div>
+        <div className='page__content'>
+          <div className='dashboard__page__content__wrapper'>
+            <button className='dashboard__page__tilebtn'>
+              <Link to={endpoints.TASKS}>Tasks</Link>
+            </button>
+            <button className='dashboard__page__tilebtn'>
+              <Link to={endpoints.TASKS}>Members</Link>
+            </button>
+          </div>
+        </div>
     </div>
-  )
+  </div>
+)
 }
