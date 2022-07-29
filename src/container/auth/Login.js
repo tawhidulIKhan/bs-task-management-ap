9a4810e757f9f2f38115bd1ad214eca38eca3573
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-29 11:09:41 +0600

    set login form handling on login component

--- src/container/auth/Login.js
+++ src/container/auth/Login.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
+import AuthManager from '../../services/api/auth/request';
 import './Auth.scss';
 
 export default function LoginContent() {
+  const [userInput, setUserInput] = useState({
+    username:'',
+    password: ''
+  });
+
+  const nameHandle = (ev) => {
+    setUserInput({
+      ...userInput,
+      username: ev.target.value
+    });
+  }
+
+  const passwordHandle = (ev) => {
+    setUserInput({
+      ...userInput,
+      password: ev.target.value
+    });
+  }
+
+  const login = async (ev) => {
+    ev.preventDefault();
+    try {
+      const response = await AuthManager.login(userInput);
+      console.log(response);
+    } catch (error) {
+      console.error(error);  
+    }
+  }
+
   return (
       <div className='auth'>
           <div className='auth__content'>
@@ -9,13 +39,13 @@ export default function LoginContent() {
             <form>
               <p>
                 <label className='auth__label'>Username</label>
-            <input className='auth__input--text input-field' placeholder='Enter your username' />
+                <input onChange={nameHandle} className='auth__input--text input-field' placeholder='Enter your username' />
               </p>
               <p>
-            <label className='auth__label'>Password</label>
-            <input className='auth__input--password input-field'  placeholder='Enter your password' />
+                <label className='auth__label'>Password</label>
+                <input onChange={passwordHandle} className='auth__input--password input-field'  placeholder='Enter your password' />
               </p>
-              <button className='btn--primary'>Login</button>
+              <button onClick={login} className='btn--primary'>Login</button>
             </form>
           </div>
       </div>
