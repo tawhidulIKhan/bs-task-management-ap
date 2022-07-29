64bdbbad3a1fb983e62c223c4e8a10f26af02f2e
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-29 21:28:55 +0600

    refactor: add page header to task list

--- src/components/tasksList/TaskList.js
+++ src/components/tasksList/TaskList.js
@@ -2,33 +2,31 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import endpoints from '../../config/endpoints'
 import TaskManager from '../../services/api/tasks/request';
+import PageHeader from '../pageHeader/PageHeader';
 
 export default function TaskList() {
   const [tasks, setTasks] = useState([])
+
   useEffect(() => {
     fetchTasks();
   }, [])
-
-  /**
-   * function to fetch all tasks
-   */
 
   const fetchTasks = async () => {
     try {
       const response = await TaskManager.all();
       setTasks(response.data);
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   }
+
   return (
     <div className='container'>
-      <div className='page__header'>
-        <h2 className='page__header__title'>All Tasks</h2>
-        <div>
-          <Link to={endpoints.TASKS_CREATE} className='btn--link'>Create New Task</Link>
-        </div>
-      </div>
+      <PageHeader 
+        title="All Task" 
+        btnLabel="Create New Task" 
+        btnLink={endpoints.TASKS_CREATE}
+      />
       <div className='page__content'>
         <table className="table">
           <thead>
