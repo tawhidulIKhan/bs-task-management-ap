603d17b6ec305777848a5131a8e12ef6fe6f4eab
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-29 20:20:11 +0600

    style: change pageheader text style

--- src/components/pageHeader/PageHeader.js
+++ src/components/pageHeader/PageHeader.js
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom'
+import endpoints from '../../config/endpoints'
+import './PageHeader.scss';
 
 export default function PageHeader(props) {
     const { title, linkLabel, link } = props
   return (
         <div className='page__header'>
         <h2 className='page__header__title'>{title}</h2>
-            <div>
+            <div className='page__header__breadcrumb'>
                 <Link to={link}>{linkLabel}</Link>
             </div>
         </div>
