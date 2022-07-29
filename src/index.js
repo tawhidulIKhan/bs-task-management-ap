2ced16edc1f6010e5b82f6256595cdacf0fd7ed4
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-29 11:57:33 +0600

    feat: add redux persist

--- src/index.js
+++ src/index.js
@@ -4,15 +4,20 @@ import './styles/app.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
-import { store } from './store'
+import store from './store'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+let persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App/>
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit'
+import storageManager from '../services/storage.js';
 import SessionReducer from './session/reducer.js';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import {combineReducers} from "redux"; 
+const reducers = combineReducers({
+  session: SessionReducer
+});
 
-export const store = configureStore({
-  reducer: {
-    session: SessionReducer
-  },
+const persistConfig = {
+  key: 'test-bs-state',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+
+
+const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
 })
+
+export default store;
