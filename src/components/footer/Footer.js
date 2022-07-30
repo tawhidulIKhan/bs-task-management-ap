7261472b54c08eefddfe549e48165d2f6d24a5eb
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-30 13:22:20 +0600

    feat: add footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Footer.scss';
+
+export default function Footer() {
+  return (
+    <footer className="footer">Copyright @ BS Task Management Web ap</footer>
+  );
+}
