c7d5176d0cf80a424208d7ead3cc7a12980e5dd6
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-30 12:34:32 +0600

    refactor: add comments on helper config

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,11 @@
 import store from '../store';
 import { getSessionToken } from '../store/session/actions';
 
+/**
+ * Get axios header object for all axios request
+ * @returns {object} object with Accept, Authorization with bearer token
+ */
+
 export const getAxiosHeader = () => {
   const token = getSessionToken(store.getState());
   return {
@@ -9,6 +14,10 @@ export const getAxiosHeader = () => {
   };
 };
 
+/**
+ * Set success message in notifier
+ * @param {string} message
+ */
 export const successMsg = (msg) => {
   const container = document.querySelector('#notification-container');
   const notification = `<div>${msg}</div>`;
