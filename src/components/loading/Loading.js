a5a8db57db1dea21861302e982e6dc0ba0477ee7
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-30 11:08:09 +0600

    refactor: add custom loading spinner

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -1,22 +1,8 @@
-import React from 'react'
-import { Oval } from 'react-loader-spinner';
 import './Loading.scss';
 export default function Loading() {
   return (
     <div className='loading'>
-        <Oval
-            height={80}
-            width={80}
-            color="#3E02BD"
-            wrapperStyle={{}}
-            wrapperClass=""
-            visible={true}
-            ariaLabel='oval-loading'
-            secondaryColor="#3E02BD"
-            strokeWidth={2}
-            strokeWidthSecondary={2}
-
-        />
+        <div className='loading__spinner'></div>
     </div>
   )
 }
