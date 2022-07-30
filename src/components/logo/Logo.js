45574f5a389a9ccfde9968ed8edd03ed8e2d2660
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-30 13:52:16 +0600

    add dashboad link to logo

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,14 +1,18 @@
+import { Link } from 'react-router-dom';
 import AppConfig from '../../config/app';
+import endpoints from '../../config/endpoints';
 import LogoBrand from './../../images/logo.svg';
 import './Logo.scss';
 
 export default function Logo() {
   return (
     <div className="logo">
-      <div>
-        <img src={LogoBrand} alt="Logo" width={30} />
-      </div>
-      <span className="logo__appname">{AppConfig.appName}</span>
+      <Link to={endpoints.DASHBOARD}>
+        <div>
+          <img src={LogoBrand} alt="Logo" width={30} />
+        </div>
+        <span className="logo__appname">{AppConfig.appName}</span>
+      </Link>
     </div>
   );
 }
