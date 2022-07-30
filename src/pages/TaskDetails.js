dadbf5833faef12b3fde0f69ebdfe399bcadf5c9
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-30 11:10:13 +0600

    refactor: add task details functionalities on task details page

--- src/pages/TaskDetails.js
+++ src/pages/TaskDetails.js
@@ -1,11 +1,35 @@
-import React from 'react'
-import Layout from '../components/layout/Layout'
-import TaskDetailsContent from '../components/taskDetailsContent/TaskDetailsContent'
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Layout from '../components/layout/Layout';
+import Loading from '../components/loading/Loading';
+import TaskForm from '../components/taskForm/TaskForm';
+import TaskManager from '../services/api/tasks/request';
 
 export default function TaskDetails() {
+  const {id} = useParams();
+  const [task, setTask] = useState(null)
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    fetchTask(id)
+  }, [id])
+  
+  const fetchTask = async (id) => {
+    try {
+      setLoading(true);
+      const response = await TaskManager.show({
+        id: id
+      });
+      setTask(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  }
+
   return (
     <Layout>
-      <TaskDetailsContent />
+      {loading ? <Loading /> : <TaskForm task={task} />}
     </Layout>
   )
 }
