3d01a815828d1ae1f17053c95256f974ce99e813
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-30 12:35:35 +0600

    fix: add key on error map

--- src/components/error/Error.js
+++ src/components/error/Error.js
@@ -4,8 +4,8 @@ import './Error.scss';
 export default function Error(props) {
   return (
     <div className="error">
-      {props.error.map((er) => (
-        <span>{er}</span>
+      {props.error.map((er, index) => (
+        <span key={index}>{er}</span>
       ))}
     </div>
   );
