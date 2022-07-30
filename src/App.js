7fffaab9dc959b21b7f4f38e0a90818884adf20f
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-30 11:35:30 +0600

    refactor: remove routes and add import of routes file

--- src/App.js
+++ src/App.js
@@ -1,64 +1,15 @@
 import { Route, Routes } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import endpoints from './config/endpoints';
-import GuestRoute from "./container/GuestRoute";
-import PrivateRoute from "./container/PrivateRoute";
-import Dashboard from './pages/Dashboard';
-import Login from './pages/Login';
-import Members from './pages/Members';
-import TaskCreate from './pages/TaskCreate';
-import TaskDetails from "./pages/TaskDetails";
-import Tasks from './pages/Tasks';
 import 'react-toastify/dist/ReactToastify.css';
-import MemberCreate from "./pages/MemberCreate";
-import MemberDetails from "./pages/MemberDetails";
+import NotificationContainer from "./components/notificationContainer/NotificationContainer";
+import routes from "./routes";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={
-          <PrivateRoute>
-            <Dashboard />
-          </PrivateRoute>
-        } />
-        <Route path={endpoints.LOGIN} element={
-          <GuestRoute>
-            <Login />
-          </GuestRoute>
-        } />
-        <Route path={endpoints.TASKS} element={
-          <PrivateRoute>
-            <Tasks />
-          </PrivateRoute>
-        } />
-        <Route path={endpoints.TASKS_DETAILS} element={
-          <PrivateRoute>
-            <TaskDetails />
-          </PrivateRoute>
-        } />
-        <Route path={endpoints.TASKS_CREATE} element={
-          <PrivateRoute>
-            <TaskCreate />
-          </PrivateRoute>
-        } />
-        <Route path={endpoints.MEMBERS} element={
-          <PrivateRoute>
-            <Members />
-          </PrivateRoute>
-        } />
-         <Route path={endpoints.MEMBERS_DETAILS} element={
-          <PrivateRoute>
-            <MemberDetails />
-          </PrivateRoute>
-        } />
-        <Route path={endpoints.MEMBERS_CREATE} element={
-          <PrivateRoute>
-            <MemberCreate />
-          </PrivateRoute>
-        } />
+        { routes.map((route, index) => <Route key={index} path={route.path} element={route.element} />)}
       </Routes>
-      <ToastContainer />
+      <NotificationContainer />
     </div>
   );
 }
