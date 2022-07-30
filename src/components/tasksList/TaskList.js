9c90662ee169dd004f778ca036f22bdd8bf88af6
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-30 15:58:56 +0600

    add loading to tasklist

--- src/components/tasksList/TaskList.js
+++ src/components/tasksList/TaskList.js
@@ -2,12 +2,14 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import endpoints from '../../config/endpoints';
 import TaskManager from '../../services/api/tasks/request';
+import Loading from '../loading/Loading';
 import NotFound from '../notFound/NotFound';
 import PageHeader from '../pageHeader/PageHeader';
 import Pagination from '../pagination/Pagination';
 
 export default function TaskList() {
   const [tasks, setTasks] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [meta, setMeta] = useState({
     currentPage: 1,
   });
@@ -18,11 +20,14 @@ export default function TaskList() {
 
   const fetchTasks = async (request) => {
     try {
+      setLoading(true);
       const response = await TaskManager.all(request);
       setTasks(response.data);
       setMeta(response.meta);
+      setLoading(false);
     } catch (error) {
       console.error(error);
+      setLoading(false);
     }
   };
 
@@ -38,31 +43,38 @@ export default function TaskList() {
         btnLink={endpoints.TASKS_CREATE}
       />
       <div className="page__content">
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Description</th>
-              <th>Assigned to</th>
-              <th>Creation date</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tasks.map((task) => (
-              <tr key={task.id}>
-                <td>
-                  <Link to={endpoints.TASKS_DETAILS.replace(':id', task.id)}>
-                    {task.title}
-                  </Link>
-                </td>
-                <td>{task.description || '-'}</td>
-                <td>{task.assignedMember}</td>
-                <td>{task.createdAt}</td>
+        {tasks.length ? (
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Assigned to</th>
+                <th>Creation date</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-        {!tasks.length ? <NotFound message="No Tasks Found" /> : null}
+            </thead>
+            <tbody>
+              {tasks.map((task) => (
+                <tr key={task.id}>
+                  <td>
+                    <Link to={endpoints.TASKS_DETAILS.replace(':id', task.id)}>
+                      {task.title}
+                    </Link>
+                  </td>
+                  <td>{task.description || '-'}</td>
+                  <td>{task.assignedMember}</td>
+                  <td>{task.createdAt}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <Loading />
+        )}
+
+        {!tasks.length && !loading ? (
+          <NotFound message="No Tasks Found" />
+        ) : null}
         {!tasks.length ? (
           <Pagination
             onClick={onPaginate}
