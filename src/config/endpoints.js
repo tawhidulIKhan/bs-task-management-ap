8a2b0cb7bbf329e4fd78d80b27440d885364e354
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-30 12:44:25 +0600

    refactor: restructure endpoints file

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -1,23 +1,32 @@
 const API_ENDPOINT = process.env.REACT_APP_API_URL;
 
-const endpoints = {
+const endpointsLocal = {
   LOGIN: '/login',
-  LOGIN_API: `${API_ENDPOINT}/login`,
   DASHBOARD: '/',
   TASKS: '/tasks',
-  TASKS_API: `${API_ENDPOINT}/tasks`,
   TASKS_DETAILS: '/tasks/:id',
+  TASKS_CREATE: '/tasks/create',
+  MEMBERS: '/members',
+  MEMBERS_DETAILS: '/members/:id',
+  MEMBERS_CREATE: '/members/create',
+};
+
+const endpointsServer = {
+  LOGIN_API: `${API_ENDPOINT}/login`,
+  TASKS_API: `${API_ENDPOINT}/tasks`,
   TASKS_DETAILS_API: `${API_ENDPOINT}/tasks/:id`,
   TASKS_UPDATE_API: `${API_ENDPOINT}/tasks/:id`,
-  TASKS_CREATE: '/tasks/create',
   TASKS_CREATE_API: `${API_ENDPOINT}/tasks/create`,
-  MEMBERS: '/members',
   MEMBERS_API: `${API_ENDPOINT}/members`,
   MEMBERS_ALL_API: `${API_ENDPOINT}/members/all`,
-  MEMBERS_DETAILS: '/members/:id',
+  MEMBERS_CREATE_API: `${API_ENDPOINT}/members/create`,
   MEMBERS_DETAILS_API: `${API_ENDPOINT}/members/:id`,
   MEMBERS_UPDATE_API: `${API_ENDPOINT}/members/:id`,
-  MEMBERS_CREATE: '/members/create',
-  MEMBERS_CREATE_API: `${API_ENDPOINT}/members/create`
 };
+
+const endpoints = {
+  ...endpointsLocal,
+  ...endpointsServer,
+};
+
 export default endpoints;
