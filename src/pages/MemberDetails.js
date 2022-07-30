71e64b5ee7c3df45853fd97ca8626513f1fa1be4
tawhidulIKhan <tawhid.developer@gmail.com>
2022-07-30 11:09:44 +0600

    refactor: add loading in member details

--- src/pages/MemberDetails.js
+++ src/pages/MemberDetails.js
@@ -1,10 +1,34 @@
-import Layout from '../components/layout/Layout'
-import MemberDetailsContent from '../components/memberDetailsContent/MemberDetailsContent'
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Layout from '../components/layout/Layout';
+import Loading from '../components/loading/Loading';
+import MemberForm from '../components/memberForm/MemberForm';
+import MemberManager from '../services/api/members/request';
 
 export default function MemberDetails() {
+  const {id} = useParams();
+  const [member, setMember] = useState(null)
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    fetchMember(id)
+  }, [id])
+  
+  const fetchMember = async (id) => {
+    try {
+      setLoading(true);
+      const response = await MemberManager.show({
+        id: id
+      });
+      setMember(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  }
   return (
     <Layout>
-      <MemberDetailsContent />
-    </Layout>
+      {loading ? <Loading /> : <MemberForm task={member} />}
+   </Layout>
   )
 }
